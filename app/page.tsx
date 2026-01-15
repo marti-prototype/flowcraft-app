967f9cef1fa327ab70199df9d26d31edc1c538f1
marti-prototype <marcinkosinski.it@gmail.com>
2026-01-15 21:48:46 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 'use client'
 
+import { useState, useEffect } from 'react'
 import TaskFlowApp from './app'
 
-export default function Home() {
+export default function Page() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setIsLoading(false)
+  }, [])
+
+  if (isLoading) {
+    return <div style={{ padding: '40px', textAlign: 'center' }}>FlowCraft App is Loading...</div>
+  }
+
   return <TaskFlowApp />
 }
